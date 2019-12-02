4d9fee38accef6ccc59d4563c5905d52d63d1a6f
NightFore <38656758+NightFore@users.noreply.github.com>
2019-12-02 01:08:33 +0100

    [Game Project 8] Labyrinth of Elrualia v0.03b - Map Events - Clean & Update(init, status) & Remove(init_new, player_status) Bug Fixes(map_001)

--- [Game Project 8] Labyrinth of Elrualia.c
+++ [Game Project 8] Labyrinth of Elrualia.c
@@ -5,123 +5,36 @@
     /* Prototyping */
 int PLAYING = 1;
 void init();
-void init_new();
-void run();
 void events();
 void player_movement();
 void init_map();
 void draw();
 void draw_map();
-void player_status();
+void status();
 int player_collide();
 
     /* Game Settings */
 char PROJECT_TITLE[] = "Labyrinth of Elrualia";
-int WIDTH = 800; int HEIGHT = 600;
-int FPS = 60;
-int TILESIZE = 32;
-int GRIDWIDTH;
-int GRIDHEIGHT;
 
 
     /* Player Settings*/
-char PLAYER_IMG[]       = "character_pipoya_male_01_2.png";
-int PLAYER_INDEX        = 1;
-int PLAYER_HIT_RECT[]   = {0, 0, 35, 35};
-int PLAYER_HEALTH       = 3;
-int PLAYER_SPEED        = 300;
+int PLAYER_HEALTH       = 10;
 int PLAYER_COIN         = 0;
 int PLAYER_KEY          = 0;
 
 
-
-    /* Mob Settings */
-char MOB_IMG[]          = "Mobs_enemy_04_1.png";
-int MOB_HIT_RECT[]      = {0, 0, 30, 30};
-int MOB_HEALTH          = 2;
-int MOB_SPEED           = 125;
-int MOB_DAMAGE          = 1;
-int MOB_KNOCKBACK       = 20;
-int MOB_RADIUS          = 30;
-int MOB_DETECT_RADIUS   = 300;
-
-
-    /* Sword Settings */
-char SWORD_IMG[]        = "Sword_PixelHole_x2.png";
-int SWORD_HIT_RECT[]    = {0, 0, 30, 30};
-int SWORD_SPEED         = 50;
-int SWORD_DAMAGE        = 1;
-int SWORD_KNOCKBACK     = 20;
-int SWORD_LIFETIME      = 300;
-int SWORD_RATE          = 500;
-/* int SWORD_OFFSET[] = vec(20, 0); */
-
-
-    /* Tweening */
-int BOB_RANGE = 10;
-int BOB_SPEED = 0.3;
-
-
-    /* Layer Settings */
-int LAYER_WALL      = 1;
-int LAYER_ITEMS     = 1;
-int LAYER_PLAYER    = 2;
-int LAYER_MOB       = 2;
-int LAYER_SWORD     = 3;
-int LAYER_EFFECTS   = 4;
-
-
-    /* Item Directories */
-/* ITEM_IMAGES[] = {"heart": ["items_beyonderboy_heart_1.png"], "coin": ["items_beyonderboy_coin_1.png", "items_beyonderboy_coin_2.png", "items_beyonderboy_coin_3.png", "items_beyonderboy_coin_4.png", "items_beyonderboy_coin_5.png", "items_beyonderboy_coin_6.png"]}*/
-char ITEM_DROPS[][100] = { {"heart"}, {"coin"} };
-char IMAGE_HEART[]  = "items_beyonderboy_heart.png";
-char IMAGE_COIN[]   = "items_beyonderboy_coin.png";
-
-
-    /* Item Settings */
-int HEART_AMOUNT = 1;
-int COIN_AMOUNT = 1;
-
-
-    /* Effect Settings*/
-/* EFFECT_IMAGES   = {"pick_up": ["effect_beyonderboy_pick_up_item_1.png", "effect_beyonderboy_pick_up_item_2.png", "effect_beyonderboy_pick_up_item_3.png", "effect_beyonderboy_pick_up_item_4.png", "effect_beyonderboy_pick_up_item_5.png", "effect_beyonderboy_pick_up_item_6.png"]} */
-
-
-    /* Sound Directories */
-char BG_MUSIC[]                 = "music_aaron_krogh_310_world_map.mp3";
-char SOUNDS_PICK_UP[]           = "sfx_maoudamashii_system23.wav";
-char SOUNDS_SWORD_ATTACK[][100] = {"Battle_Slash_battle01.wav", "Battle_Slash_battle03.wav", "Battle_Slash_battle17.wav"};
-char VOICE_PLAYER_ATTACK[][100] = {"voice_wingless_seraph_jakigan_07_attack.wav", "voice_wingless_seraph_jakigan_08_attack.wav"};
-char VOICE_PLAYER_DAMAGE[][100] = {"voice_wingless_seraph_jakigan_14_damage.wav", "voice_wingless_seraph_jakigan_15_damage.wav", "voice_wingless_seraph_jakigan_16_damage.wav"};
-
-
-    /* Colors */
-int RED[]           = {255, 0,   0};
-int C_GREEN[]       = {0,   255, 0};
-int C_DARKGREEN[]   = {60,  210, 120};
-int C_BLUE[]        = {0,   0,   255};
-int C_LIGHTBLUE[]   = {140, 205, 245};
-int C_YELLOW[]      = {255, 255, 0};
-int C_CYAN[]        = {0,  255,  255};
-int C_GREY[]        = {150, 170, 210};
-int C_LIGHTGREY[]   = {100, 100, 100};
-int C_BLACK[]       = {0,   0,   0};
-int C_WHITE[]       = {255, 255, 255};
-int C_BGCOLOR[]     = {200, 200, 200};
-int C_INTERFACE[]   = {140, 205, 245};
-
-
     /* Global Variables */
-int player_position[] = {10, 3};
+int player_position[] = {0, 0};
 
 
     /* Temporary */
-int map_1[20][20] =
+int map_001[20][20] =
     {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 5},
     {0, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 8, 0, 0, 0, 0, 0, 3, 0, 0, 3},
@@ -146,29 +59,9 @@ int map_1[20][20] =
 /* ----- Main Functions ----- */
 
 /* ------------------------------ */
-
 int main()
 {
     init();
-    init_new();
-    run();
-}
-
-void init()
-{
-    draw_map();
-
-    GRIDWIDTH   = WIDTH/TILESIZE;
-    GRIDHEIGHT  = HEIGHT / TILESIZE;
-}
-
-void init_new()
-{
-
-}
-
-void run()
-{
     while (PLAYING==1)
     {
         events();
@@ -176,16 +69,20 @@ void run()
     }
 }
 
+void init()
+{
+    draw_map();
+}
+
 void events()
 {
     player_movement();
 }
 
-
 void draw()
 {
     draw_map();
-    player_status();
+    status();
 }
 
 
@@ -205,7 +102,7 @@ void draw_map()
         {
             if (index_l != player_position[0] || index_c != player_position[1])
             {
-                printf("%d ", map_1[index_l][index_c]);
+                printf("%d ", map_001[index_l][index_c]);
             }
             else
             {
@@ -219,12 +116,24 @@ void draw_map()
 
 
 
-void player_status()
+void status()
 {
     printf("Health: %d\n", PLAYER_HEALTH);
-    printf("Coin(s): %d\n", PLAYER_COIN);
+    printf("Coin(s): %d / 10\n", PLAYER_COIN);
     printf("Key(s): %d\n", PLAYER_KEY);
     printf("\n");
+
+    if (PLAYER_COIN >= 10)
+    {
+        printf("You've win! Game Over!\n");
+        PLAYING = 0;
+    }
+
+    if (PLAYER_HEALTH <= 0)
+    {
+        printf("You've died! Game Over!\n");
+        PLAYING = 0;
+    }
 }
 
 void player_movement()
@@ -296,7 +205,7 @@ int player_collide()
 
 
     /* 0: Grass | 1: Flower | 2: Tree | 3: Rock | 4: Key | 5: Coin | 6: Lock | 7: Trap | 8: Monster */
-    int p_p = map_1[player_position[0]][player_position[1]];
+    int p_p = map_001[player_position[0]][player_position[1]];
     if (p_p == 0)
     {
         printf("You walked on Grass.\n");
@@ -323,7 +232,7 @@ int player_collide()
 
     if (p_p == 4)
     {
-        map_1[player_position[0]][player_position[1]]= 0;
+        map_001[player_position[0]][player_position[1]]= 0;
         printf("You found a Key on the ground!\n");
         PLAYER_KEY++;
         return 1;
@@ -331,7 +240,7 @@ int player_collide()
 
     if (p_p == 5)
     {
-        map_1[player_position[0]][player_position[1]]= 0;
+        map_001[player_position[0]][player_position[1]]= 0;
         printf("You found a Coin on the ground!\n");
         PLAYER_COIN++;
         return 1;
@@ -342,7 +251,7 @@ int player_collide()
         printf("You found a Lock on your way!\n");
         if (PLAYER_KEY > 0)
         {
-            map_1[player_position[0]][player_position[1]]= 0;
+            map_001[player_position[0]][player_position[1]]= 0;
             printf("You've used a Key to open the Lock!");
             PLAYER_KEY--;
             return 1;
@@ -356,7 +265,7 @@ int player_collide()
 
     if (p_p == 7)
     {
-        map_1[player_position[0]][player_position[1]]= 0;
+        map_001[player_position[0]][player_position[1]]= 0;
         printf("You stepped on a trap!\nYou lost 1 HP!\n");
         PLAYER_HEALTH--;
         return 1;
@@ -364,7 +273,7 @@ int player_collide()
 
     if (p_p == 8)
     {
-        map_1[player_position[0]][player_position[1]]= 0;
+        map_001[player_position[0]][player_position[1]]= 0;
         printf("You met a monster on your way!\nYou lost 1 HP by fighting him!\n");
         PLAYER_HEALTH--;
         return 1;
