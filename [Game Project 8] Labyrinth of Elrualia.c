8f61a7e271efe4385e3750c2560810901a8de363
NightFore <38656758+NightFore@users.noreply.github.com>
2019-12-13 21:02:47 +0100

    [Game Project 8] Labyrinth of Elrualia v0.1.0.b - SDL - Update(SDL_timer, SDL_INIT_TIMER, SDL_Window, SDL_CreateWindow)

--- [Game Project 8] Labyrinth of Elrualia.c
+++ [Game Project 8] Labyrinth of Elrualia.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    /** Initialization Error **/
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
     {
         printf("Error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    printf("Initialization successful!\n");
+    SDL_Window* win = SDL_CreateWindow("Hello, CS50!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
 
+    /** Winwdow Error **/
+    if (!win)
+    {
+        printf("Error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    /** Wait a few seconds **/
+    SDL_Delay(5000);
+
+    /** Clean up **/
+    SDL_DestroyWindow(win);
     SDL_Quit();
 }
 
