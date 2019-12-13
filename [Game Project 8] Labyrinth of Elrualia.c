1478211d8dacbea524800c0740d3f548e7da8355
NightFore <38656758+NightFore@users.noreply.github.com>
2019-12-13 22:18:53 +0100

    [Game Project 8] Labyrinth of Elrualia v0.1.0.c - SDL - Update(SDL_image, SDL_Renderer, SDL_Surface, SDL_Texture, hello)

--- [Game Project 8] Labyrinth of Elrualia.c
+++ [Game Project 8] Labyrinth of Elrualia.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
 
 int main(int argc, char *argv[])
 {
@@ -21,10 +22,54 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    /** Renderer **/
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    if (!rend)
+    {
+        printf("Error creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    /** Load the image into memory using SDL_image library function **/
+    SDL_Surface* surface = IMG_Load("data/hello.jpg");
+    if (!surface)
+    {
+        printf("Error creating surface: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    /** Load the image data into the graphics hardware's memory **/
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if (!tex)
+    {
+        printf("Error creating surface: %s\n", SDL_GetError());
+        SDL_DestroyTexture(tex);
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    /** Clear the window **/
+    SDL_RenderClear(rend);
+
+    /** Draw the image to the window **/
+    SDL_RenderCopy(rend, tex, NULL, NULL);
+    SDL_RenderPresent(rend);
+
     /** Wait a few seconds **/
     SDL_Delay(5000);
 
     /** Clean up **/
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
