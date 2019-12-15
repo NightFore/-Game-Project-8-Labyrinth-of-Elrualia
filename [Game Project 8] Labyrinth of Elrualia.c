d0d7721af360f6a46ba5e54460e20aea611e0cd2
NightFore <38656758+NightFore@users.noreply.github.com>
2019-12-15 22:02:59 +0100

    [Game Project 8] Labyrinth of Elrualia v0.1.1.c - Main - Update(player, dest, init_SDL, init, events, draw)

--- [Game Project 8] Labyrinth of Elrualia.c
+++ [Game Project 8] Labyrinth of Elrualia.c
@@ -17,11 +17,25 @@ SDL_Texture* tex;
 
 struct
 {
+    int playing;
     int up;
     int down;
     int left;
     int right;
 } game;
+
+struct
+{
+    float x_pos;
+    float y_pos;
+
+    float x_vel;
+    float y_vel;
+} player;
+
+/* Struct to hold the position and size of the sprite */
+SDL_Rect dest;
+
 
 
 /* ------------------------------ */
@@ -31,30 +45,148 @@ struct
 /* ------------------------------ */
 int main(int argc, char *argv[])
 {
-    /** Start Game **/
     init();
-    while (!playing)
+    while (!game.playing)
     {
         events();
         draw();
     }
 
+    quit_game();
     return 0;
 }
 
 void init()
 {
+    game.playing = 0;
     srand(time(NULL));
     init_SDL();
+
+    /* Get the dimensions of the texture */
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    dest.w /= 4;
+    dest.h /= 4;
+
+    /* X & Y position */
+    player.x_pos = (WINDOW_WIDTH - dest.w) / 2;
+    player.y_pos = (WINDOW_HEIGHT - dest.h) / 2;
+
+    /* X & Y Velocity */
+    player.x_vel = 0;
+    player.y_vel = 0;
 }
 
 void events()
 {
+    /* Events */
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+        case SDL_QUIT:
+            playing = 1;
+            break;
+
+        case SDL_KEYDOWN:
+            switch(event.key.keysym.scancode)
+            {
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                game.up = 1;
+                break;
+
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+                game.down = 1;
+                break;
+
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                game.left = 1;
+                break;
+
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                game.right = 1;
+                break;
+            }
+            break;
+
+        case SDL_KEYUP:
+            switch(event.key.keysym.scancode)
+            {
+            case SDL_SCANCODE_W:
+            case SDL_SCANCODE_UP:
+                game.up = 0;
+                break;
+
+            case SDL_SCANCODE_S:
+            case SDL_SCANCODE_DOWN:
+                game.down = 0;
+                break;
+
+            case SDL_SCANCODE_A:
+            case SDL_SCANCODE_LEFT:
+                game.left = 0;
+                break;
+
+            case SDL_SCANCODE_D:
+            case SDL_SCANCODE_RIGHT:
+                game.right = 0;
+                break;
+            }
+            break;
+        }
+    }
+
+    /* Determine velocity */
+    player.x_vel = player.y_vel = 0;
+    if (game.up && !game.down) player.y_vel = -SPEED;
+    if (game.down && !game.up) player.y_vel = SPEED;
+    if (game.left && !game.right) player.x_vel = -SPEED;
+    if (game.right && !game.left) player.x_vel = SPEED;
+
+    /* Update position */
+    player.x_pos += player.x_vel / 60;
+    player.y_pos += player.y_vel / 60;
+
+    /* Collision with bounds */
+    if (player.x_pos <= 0) player.x_pos = 0;
+    if (player.y_pos <= 0) player.y_pos = 0;
+    if (player.x_pos >= WINDOW_WIDTH - dest.w) player.x_pos = WINDOW_WIDTH - dest.w;
+    if (player.y_pos >= WINDOW_HEIGHT - dest.h) player.y_pos = WINDOW_HEIGHT - dest.h;
+
+    /* Set position in the struct */
+    dest.x = (int) player.x_pos;
+    dest.y = (int) player.y_pos;
 }
 
 void draw()
 {
+    /* Clear the window */
+    SDL_RenderClear(rend);
+
+    /* Draw image to the window */
+    SDL_RenderCopy(rend, tex, NULL, &dest);
+    SDL_RenderPresent(rend);
+
+    /* 60 FPS */
+    SDL_Delay(1000/60);
 }
+
+void quit_game()
+{
+    /** Wait a few seconds **/
+    SDL_Delay(1000);
+
+    /** Clean up **/
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
+
 int init_SDL()
 {
     /** Initialization **/
@@ -64,9 +196,9 @@ int init_SDL()
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Hello, CS50!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    win = SDL_CreateWindow("Hello, CS50!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
 
-    /** Winwdow **/
+    /** Window **/
     if (!win)
     {
         printf("Error creating window: %s\n", SDL_GetError());
@@ -76,7 +208,7 @@ int init_SDL()
 
     /** Renderer **/
     Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    rend = SDL_CreateRenderer(win, -1, render_flags);
     if (!rend)
     {
         printf("Error creating renderer: %s\n", SDL_GetError());
@@ -86,7 +218,7 @@ int init_SDL()
     }
 
     /** Surface **/
-    SDL_Surface* surface = IMG_Load("data/hello.jpg");
+    surface = IMG_Load("data/hello.jpg");
     if (!surface)
     {
         printf("Error creating surface: %s\n", SDL_GetError());
@@ -97,7 +229,7 @@ int init_SDL()
     }
 
     /** Texture **/
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    tex = SDL_CreateTextureFromSurface(rend, surface);
     SDL_FreeSurface(surface);
     if (!tex)
     {
@@ -108,128 +240,5 @@ int init_SDL()
         SDL_Quit();
         return 1;
     }
-
-    /** Animation **/
-    /* Struct to hold the position and size of the sprite */
-    SDL_Rect dest;
-
-    /* Get the dimensions of the texture */
-    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
-    dest.w /= 4;
-    dest.h /= 4;
-
-    /* X & Y position */
-    float x_pos = (WINDOW_WIDTH - dest.w) / 2;
-    float y_pos = (WINDOW_HEIGHT - dest.h) / 2;
-
-    /* X & Y Velocity */
-    float x_vel = 0;
-    float y_vel = 0;
-
-    /** Animation Loop **/
-    while (!playing)
-    {
-        /* Events */
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch(event.type)
-            {
-            case SDL_QUIT:
-                playing = 1;
-                break;
-
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    game.up = 1;
-                    break;
-
-                case SDL_SCANCODE_S:
-                case SDL_SCANCODE_DOWN:
-                    game.down = 1;
-                    break;
-
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    game.left = 1;
-                    break;
-
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    game.right = 1;
-                    break;
-                }
-                break;
-
-            case SDL_KEYUP:
-                switch(event.key.keysym.scancode)
-                {
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    game.up = 0;
-                    break;
-
-                case SDL_SCANCODE_S:
-                case SDL_SCANCODE_DOWN:
-                    game.down = 0;
-                    break;
-
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    game.left = 0;
-                    break;
-
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    game.right = 0;
-                    break;
-                }
-                break;
-            }
-        }
-
-        /* Determine velocity */
-        x_vel = y_vel = 0;
-        if (game.up && !game.down) y_vel = -SPEED;
-        if (game.down && !game.up) y_vel = SPEED;
-        if (game.left && !game.right) x_vel = -SPEED;
-        if (game.right && !game.left) x_vel = SPEED;
-
-        /* Update position */
-        x_pos += x_vel / 60;
-        y_pos += y_vel / 60;
-
-        /* Collision with bounds */
-        if (x_pos <= 0) x_pos = 0;
-        if (y_pos <= 0) y_pos = 0;
-        if (x_pos >= WINDOW_WIDTH - dest.w) x_pos = WINDOW_WIDTH - dest.w;
-        if (y_pos >= WINDOW_HEIGHT - dest.h) y_pos = WINDOW_HEIGHT - dest.h;
-
-        /* Set position in the struct */
-        dest.x = (int) x_pos;
-        dest.y = (int) y_pos;
-
-        /* Clear the window */
-        SDL_RenderClear(rend);
-
-        /* Draw image to the window */
-        SDL_RenderCopy(rend, tex, NULL, &dest);
-        SDL_RenderPresent(rend);
-
-        /* 60 FPS */
-        SDL_Delay(1000/60);
-    }
-
-    /** Wait a few seconds **/
-    SDL_Delay(1000);
-
-    /** Clean up **/
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
 }
 
